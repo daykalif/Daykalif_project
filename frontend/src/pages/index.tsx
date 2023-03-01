9617d8bd4c91681cb401ed4e5c9d9a16fe0d132f
daykalif <1038200595@qq.com>
2023-03-01 23:39:29 +0800

    feat:添加lodash

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,16 @@
+import _ from 'lodash'
+import { add } from 'lodash/fp'
+import { useEffect } from 'react';
+
 import styles from './index.less';
 
 export default function IndexPage() {
+  useEffect(() => {
+    const addOne = add(5);
+    const lodash_test = _.map([1, 2, 3], addOne);
+    console.log(lodash_test);
+  }, []);
+
   return (
     <div>
       <h1 className={styles.title}>Page index</h1>
