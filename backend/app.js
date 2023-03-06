99c49cb380e77873cdaf84808dc831fda7c0d83a
daykalif <1038200595@qq.com>
2023-03-06 23:21:22 +0800

    feat:探究中间件的执行逻辑

--- backend/app.js
+++ backend/app.js
@@ -27,6 +27,84 @@ app.use('/users', usersRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 
+
+
+// ---------------探究中间件的执行逻辑----------------
+app.use((req, res, next) => {
+  console.log('请求开始...', req.method, req.url);
+  next()
+});
+
+app.use((req, res, next) => {
+  // 假设设置cookie
+  req.cookie = {
+    userId: 'abc123'
+  }
+  next()
+});
+
+app.use((req, res, next) => {
+  // 假设处理post data
+  // 异步
+  setTimeout(() => {
+    req.body = {
+      a: 100,
+      b: 200
+    }
+    next()
+  })
+});
+
+
+app.use('/api', (req, res, next) => {
+  console.log('处理 /api 路由')
+  next()
+});
+
+app.get('/api', (req, res, next) => {
+  console.log('get /api 路由')
+  next()
+});
+
+app.post('/api', (req, res, next) => {
+  console.log('post /api 路由')
+  next()
+});
+
+// 模拟登陆验证
+function loginCheck(req, res, next) {
+  setTimeout(() => {
+    if (true) {
+      console.log('模拟登陆成功')
+      next()
+    } else {
+      console.log('模拟登陆失败')
+      res.json({
+        errorno: -1,
+        msg: '登陆失败'
+      })
+    }
+  })
+}
+
+app.get('/api/get-cookie', loginCheck, (req, res, next) => {    // 访问：http://localhost:3000/api/get-cookie
+  console.log('get /api/get-cookie')
+  res.json({
+    errorno: 0,
+    data: req.cookie
+  })
+});
+
+app.post('/api/get-post-data', loginCheck, (req, res, next) => {    // postman访问：http://localhost:3000/api/get-post-data
+  console.log('get /api/get-post-data')
+  res.json({
+    errorno: 0,
+    data: req.body
+  })
+});
+// ---------------探究中间件的执行逻辑----------------
+
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
