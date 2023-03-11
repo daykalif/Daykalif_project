552a9addf8fdac4f8743d8685d32809ee090b865
daykalif <1038200595@qq.com>
2023-03-11 11:40:24 +0800

    update:去除无用代码

--- backend/app.js
+++ backend/app.js
@@ -7,26 +7,19 @@ const Joi = require('@hapi/joi');
 
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 const blogRouter = require('./routes/blog');
 const userRouter = require('./routes/user');
 const registerRouter = require('./routes/register');
 
 var app = express();
 
-// view engine setup  // 当前项目中前端引擎设置（由于本项目前后端分离，所以不需要使用）
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());  // 设置之后，可以在路由中通过request.body获取post传递过来的数据; post兼容application/json格式
 app.use(express.urlencoded({ extended: false })); // post兼容application/x-www-form-urlencode格式
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // 注册路由
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/api/blog', blogRouter);
 app.use('/api/user', userRouter);
 app.use('/api/user', registerRouter);
